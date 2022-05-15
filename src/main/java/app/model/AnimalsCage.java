49452e74d323584a356fc704af4c9aa3f18b2ab8
Vitaly Shishkin <67963461+shishkin91@users.noreply.github.com>
2022-05-15 23:00:03 +0300

    ✔

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,27 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
+    private Timer timer = new Timer(); // 4) На основе класса Timer создайте бин. Свяжите с AnimalCage.
+
+    public Timer getTimer() {
+        return timer;
+    }
+
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime()); // или как было через new, тест проходит в любом случае. не понятно условие про одинаковое время.
         System.out.println("________________________");
     }
 }
--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,8 +1,13 @@
 package app.model;
 
-public class Timer {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
+public class Timer {
     private Long nanoTime = System.nanoTime();
+
 
     public Long getTime() {
         return nanoTime;
